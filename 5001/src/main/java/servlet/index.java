4f827b3fb38cd6ef24cb255953336e6c8085aaa9
唐齐 <76168926+leeqi10@users.noreply.github.com>
2022-05-27 23:58:46 +0800

    Add files via upload

---
+++ 5001/src/main/java/servlet/index.java
@@ -0,0 +1,17 @@
+package servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "index", value = "/index")
+public class index extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        request.setCharacterEncoding("UTF-8");
+    }
+}
+
